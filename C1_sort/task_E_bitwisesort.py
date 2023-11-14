256a6cdd72c39cfe50ed0a699d1c3027ebed7871
Mitchell Dorren <mitcheld@ev-n5.kzn.21-school.ru>
2023-11-14 20:28:25 +0300

    bitwise sort little fix done

--- C1_sort/task_E_bitwisesort.py
+++ C1_sort/task_E_bitwisesort.py
@@ -1,16 +1,10 @@
-N = int(input())
-S = [0] * N
-for i in range(N):
-    S[i] = input()
-m = len(S[0])
-
-
 def print_dict(d, phase):
     print(f"Phase {phase}")
     for k, v in d.items():
         if len(v) != 0:
             print(k, end=" ")
-            print(*[item + (', ' if i < len(v)-1 else '') for i, item in enumerate(v)])
+            print(*[item + (',' if i < len(v)-1 else '')
+                  for i, item in enumerate(v)])
         else:
             print(k, "empty")
     print("**********")
@@ -18,36 +12,44 @@ def print_dict(d, phase):
     return phase
 
 
-print("Initial array:")
-print(*[item + (', ' if i < len(S)-1 else '') for i, item in enumerate(S)])
-print("**********")
-buckets_key = [f"Bucket {n}:" for n in range(10)]
-buckets_value = [[] for n in range(10)]
+N = int(input())
 
-buckets = dict(zip(buckets_key, buckets_value))
+if (N):
+    S = [0] * N
+    for i in range(N):
+        S[i] = input()
+    m = len(S[0])
+    print("Initial array:")
+    print(*[item + (',' if i < len(S)-1 else '') for i, item in enumerate(S)])
+    print("**********")
+    buckets_key = [f"Bucket {n}:" for n in range(10)]
+    buckets_value = [[] for n in range(10)]
 
-for i in range(len(S)):
-    e = str(S[i])
-    buckets[f"Bucket {e[m-1]}:"].append(e)
-m -= 1
-phase = 1
-phase = print_dict(buckets, phase)
+    buckets = dict(zip(buckets_key, buckets_value))
 
-while m:
-    for k, v in buckets.items():
-        shift = 0
-        for i_v in range(len(v)):
-            if v[i_v-shift][m-1] != str(i_v):
-                t = v.pop(i_v-shift)
-                buckets[f"Bucket {t[m-1]}:"].append(t)
-                shift += 1
-    phase = print_dict(buckets, phase)
+    for i in range(len(S)):
+        e = S[i]
+        buckets[f"Bucket {e[m-1]}:"].append(e)
     m -= 1
+    phase = 1
+    phase = print_dict(buckets, phase)
 
-S = []
-for k, v in buckets.items():
-    for i in v:
-        S.append(i)
+    while m:
+        for k, v in buckets.items():
+            shift = 0
+            for i_v in range(len(v)):
+                if v[i_v-shift][m-1] != str(i_v):
+                    t = v.pop(i_v-shift)
+                    buckets[f"Bucket {t[m-1]}:"].append(t)
+                    shift += 1
+        phase = print_dict(buckets, phase)
+        m -= 1
 
-print("Sorted array:")
-print(*[item + (', ' if i < len(S)-1 else '') for i, item in enumerate(S)], end="\n")
+    S = []
+    for k, v in buckets.items():
+        for i in v:
+            S.append(i)
+
+    print("Sorted array:")
+    print(*[item + (',' if i < len(S)-1 else '')
+            for i, item in enumerate(S)], end="\n")
